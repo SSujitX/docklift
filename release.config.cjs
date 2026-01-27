d5b8129f273d06425dc136b48884a21bdc2f6cae
SSujitX <ssujitxx@gmail.com>
2026-01-28 04:06:00 +0600

    ci: update release configuration and remove obsolete test workflow

--- release.config.cjs
+++ release.config.cjs
@@ -4,20 +4,37 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/git', {
-        "assets": [
-            "CHANGELOG.md",
-            "package.json",
-            "package-lock.json",
-            "npm-shrinkwrap.json",
-            "frontend/package.json",
-            "frontend/package-lock.json",
-            "backend/package.json",
-            "backend/package-lock.json",
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm', // Updates root package.json
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd:
+          'npm version ${nextRelease.version} --workspaces --no-git-tag-version --allow-same-version || (npm version ${nextRelease.version} --no-git-tag-version --prefix frontend && npm version ${nextRelease.version} --no-git-tag-version --prefix backend)',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'CHANGELOG.md',
+          'package.json',
+          'package-lock.json',
+          'npm-shrinkwrap.json',
+          'frontend/package.json',
+          'frontend/package-lock.json',
+          'backend/package.json',
+          'backend/package-lock.json',
         ],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    },
-    '@semantic-release/github'
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
   ],
 };
 
